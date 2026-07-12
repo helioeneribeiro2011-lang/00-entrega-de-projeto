c4ca9c39adb07a47893921d49d63ab6a4ce33fb5
helioeneribeiro2011-lang <helioeneribeiro2011@gmail.com>
2026-07-12 10:33:09 -0400

    chore: configure swagger and expose UI at /docs; fix duplicated route

--- project-f1-minimal/src/app.ts
+++ project-f1-minimal/src/app.ts
@@ -2,9 +2,26 @@ import fastify from "fastify";
 import cors from "@fastify/cors";
 import { registerDriversRoutes } from "./routes/drivers";
 import { registerTeamsRoutes } from "./routes/teams";
-
+import fastifySwagger from '@fastify/swagger'
+import fastifySwaggerUi from '@fastify/swagger-ui'
 export async function buildServer() {
   const server = fastify({ logger: true });
+
+  // Registra o Swagger
+  await server.register(fastifySwagger, {
+    openapi: {
+      info: {
+        title: 'Fórmula 1 API',
+        description: 'Documentação da API de Pilotos e Equipes de F1',
+        version: '1.0.0'
+      }
+    }
+  });
+
+  // Registra a interface visual na rota raiz (/)
+  await server.register(fastifySwaggerUi, {
+    routePrefix: '/docs'
+  });
 
   await server.register(cors, {
     origin: "*",
